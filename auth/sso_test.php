b22fcb0550ff36a803de1ca07496f4544c53f5d9
feodor <feodor1999@email.ru>
2025-06-11 10:14:26 +0300

    поменял фио

--- auth/sso_test.php
+++ auth/sso_test.php
@@ -3,7 +3,7 @@ require_once('../Cors.php');
 $cors = new Cors();
 $cors->cors_policy();
 $array = [
-    'fullname' => 'Кульков Вадим Владимирович',
+    'fullname' => 'Ярмолинский Федор Александрович',
     'sl_full_name' => 'Октябрьская дирекция снабжения',
     'email' => 'dmto_KulkovVV@orw.rzd'
 ];
